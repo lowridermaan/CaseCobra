ff27ecaecaae618ddb899a1a2d984ebb7206a210
Alex <lowridermaan@gmail.com>
2024-06-30 13:43:09 +0300

    поправил resend

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -74,7 +74,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "casecobra <voken43666@kinsef.com>",
+        from: "casecobra <onboarding@resend.dev>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order",
         react: OrderEmail({
